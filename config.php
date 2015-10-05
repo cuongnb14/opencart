c0a8185e468a2c90b9e8c10c8881235d385cb013
cuongnb14 <cuongnb14@gmail.com>
2015-10-05 23:52:06 +0700

    init

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8014/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8014/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/catalog/');
+define('DIR_SYSTEM', '/var/www/html/system/');
+define('DIR_LANGUAGE', '/var/www/html/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/system/config/');
+define('DIR_IMAGE', '/var/www/html/image/');
+define('DIR_CACHE', '/var/www/html/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/system/download/');
+define('DIR_UPLOAD', '/var/www/html/system/upload/');
+define('DIR_MODIFICATION', '/var/www/html/system/modification/');
+define('DIR_LOGS', '/var/www/html/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost:8014');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
