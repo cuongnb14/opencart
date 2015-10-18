7bde4a10274b5d6858c6a80ab87981caf04d47f5
cuongnb14 <cuongnb14@gmail.com>
2015-10-18 15:35:59 +0700

    update path directory

--- admin/config.php
+++ admin/config.php
@@ -8,18 +8,18 @@ define('HTTPS_SERVER', 'http://'.getenv('OC_HOST').'/admin/');
 define('HTTPS_CATALOG', 'http://'.getenv('OC_HOST').'/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/html/opencart/admin/');
-define('DIR_SYSTEM', '/var/www/html/opencart/system/');
-define('DIR_LANGUAGE', '/var/www/html/opencart/admin/language/');
-define('DIR_TEMPLATE', '/var/www/html/opencart/admin/view/template/');
-define('DIR_CONFIG', '/var/www/html/opencart/system/config/');
-define('DIR_IMAGE', '/var/www/html/opencart/image/');
-define('DIR_CACHE', '/var/www/html/opencart/system/cache/');
-define('DIR_DOWNLOAD', '/var/www/html/opencart/system/download/');
-define('DIR_UPLOAD', '/var/www/html/opencart/system/upload/');
-define('DIR_LOGS', '/var/www/html/opencart/system/logs/');
-define('DIR_MODIFICATION', '/var/www/html/opencart/system/modification/');
-define('DIR_CATALOG', '/var/www/html/opencart/catalog/');
+define('DIR_APPLICATION', '/var/www/html/admin/');
+define('DIR_SYSTEM', '/var/www/html/system/');
+define('DIR_LANGUAGE', '/var/www/html/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/system/config/');
+define('DIR_IMAGE', '/var/www/html/image/');
+define('DIR_CACHE', '/var/www/html/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/system/download/');
+define('DIR_UPLOAD', '/var/www/html/system/upload/');
+define('DIR_LOGS', '/var/www/html/system/logs/');
+define('DIR_MODIFICATION', '/var/www/html/system/modification/');
+define('DIR_CATALOG', '/var/www/html/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
